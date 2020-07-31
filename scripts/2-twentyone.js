491856b9698b933e80f6f0f101eda22cef0d336e
Ortega <ortega.victor@gmail.com>
2020-07-31 14:17:14 +0200

    Update second game

--- scripts/2-twentyone.js
+++ scripts/2-twentyone.js
@@ -1,22 +1,43 @@
-//Definig object
 
-() => {
+
+(() => {
     var deck = {
-        card=["heart", "spades", "spikes", "diamond",],
-        icon =["♥", "♠", "♣", "♦"],
-        figure=["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K"],
+        card: ["hearts", "spades", "spikes", "diamond"],
+        icon: ["♥", "♠", "♣", "♦"],
+        figure: ["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K"],
+        value: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    };
 
-        value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10"]
+    //Generate some random option for machine
+    function generator(min, max) {
+        let rndNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        return rndNumber;
     }
 
-    //one function has to be assign to objet or array with all the cards
-    function getSum(hands) {
-        var sum = 0;
-        for (var i = 0; i < hands.length; i++) {
-            if ()
-        }
+    //Creating the Deck
+    function getDeck() {
 
-    } ()
+        let innerDeck = [];
+        for (var i = 0; i < deck.card.length; i++) {
+            for (var j = 0; j < deck.figure.length; j++) {
+                innerDeck[innerDeck.length] = { card: deck.card[i], icon: deck.icon[i], figure: deck.figure[j], value: deck.value[j] }
+            }
+        }
+        return innerDeck;
+    }
+    //Picking rendomly a Card 
+    function randomCard() {
+        var randomCard = generator(51, 0);
+        return theDeck[randomCard];
+    }
+
+
+    //Execution Secuence
+    theDeck = getDeck();
+    console.table(theDeck);
+    console.log(randomCard());
+
+})();
 
 
 
