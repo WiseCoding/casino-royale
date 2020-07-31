380cd9494c166fba98a53b9d51532578168cd14d
Ortega <ortega.victor@gmail.com>
2020-07-31 12:22:01 +0200

    Start coding game #2

---
+++ scripts/2-twentyone.js
@@ -0,0 +1,23 @@
+//Definig object
+
+() => {
+    var deck = {
+        card=["heart", "spades", "spikes", "diamond",],
+        icon =["♥", "♠", "♣", "♦"],
+        figure=["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K"],
+
+        value=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10"]
+    }
+
+    //one function has to be assign to objet or array with all the cards
+    function getSum(hands) {
+        var sum = 0;
+        for (var i = 0; i < hands.length; i++) {
+            if ()
+        }
+
+    } ()
+
+
+
+
