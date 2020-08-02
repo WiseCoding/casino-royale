24290b8073f64fe5a460913d9167e68e91966f31
Mattias Bonte <wisecodr@gmail.com>
2020-08-02 23:03:07 +0200

    Add Basic Design

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// CARD Click Events
+document.querySelector('#card1').onclick = () => {
+  window.location = 'html/1-rpsls.html';
+};
+document.querySelector('#card2').onclick = () => {
+  window.location = 'html/2-twentyone.html';
+};
+document.querySelector('#card3').onclick = () => {
+  window.location = 'html/3-memory.html';
+};
+document.querySelector('#card4').onclick = () => {
+  window.location = 'html/4-blocks.html';
+};
+
+// USER Creation
