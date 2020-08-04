9bd83c1fee6d6a7d71b60535181014f6c5992da3
Mattias Bonte <wisecodr@gmail.com>
2020-08-05 00:29:17 +0200

    Start Points feature

--- scripts/player.js
+++ scripts/player.js
@@ -1,32 +1,35 @@
 (() => {
-  // GET PLAYER INFO
-  function getPlayer(credits) {
+  // LOGIN FUNCTIONALITY
+  function getPlayer(startCredits) {
     let player = localStorage.getItem('username');
     if (!player) {
       player = prompt('Please enter your name');
       document.querySelector('#userName').innerHTML = player;
       localStorage.setItem('username', player);
-      localStorage.setItem('credits', credits);
-      document.querySelector('#credits').innerHTML = credits;
+      localStorage.setItem('credits', startCredits);
+      document.querySelector('#credits').innerHTML = startCredits;
     } else {
       document.querySelector('#credits').innerHTML = localStorage.getItem('credits');
       document.querySelector('#userName').innerHTML = player;
     }
   }
 
+  // CREDITS FUNCTIONALITY
   function getCredits() {
     return localStorage.getItem('credits');
   }
-
   function addCredits(newCredits) {
     const oldCredits = localStorage.getItem('credits');
     localStorage.setItem('credits', Number(oldCredits) + newCredits);
     document.querySelector('#credits').innerHTML = localStorage.getItem('credits');
   }
 
-  // GET-SET PLAYER & STARTING CREDITS (100)
+  //
+
+  // GET+SET USERNAME & STARTING CREDITS
   getPlayer(100);
 
+  // ADD CREDITS BUTTON
   document.querySelector('#addCredits').onclick = () => {
     addCredits(100);
   };
